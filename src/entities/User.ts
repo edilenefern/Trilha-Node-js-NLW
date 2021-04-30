b2a80fbe941b4a069752aa55788f7c123b024cd7
unknown <edilenefern@gmail.com>
2021-04-30 03:24:44 -0300

    Creating ws and http protocol

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,20 +3,20 @@ import { v4 as uuid } from "uuid";
 
 @Entity("users")
 class User {
-    @PrimaryColumn()
-    id:string;
+  @PrimaryColumn()
+  id: string;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    constructor() {
-        if (!this.id) {
-            this.id = uuid();
-        }
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
     }
+  }
 }
 
 export { User };
@@ -25,3 +25,30 @@ export { User };
 // Para criar a migration: yarn typeorm migration:create -n nome da tabela
 // Para executar a migration: yarn typeorm migration:run
 // Para executar a aplicação: yarn dev
+// Biblioteca socket instalação:yarn add socket.io
+// Tipagens do Socket io como dependência de desenvolvimento:
+// yarn add @types/socket.io -D
+
+/**
+ * Exemplos de Rotas
+ * patch: Para alterar uma informação específica
+
+
+ app.get("/", (request, response) =>{
+     return response.send("Olá NLW 05");
+ })
+
+
+ app.get("/", (request, response) => {
+    return response.json({
+       message: "Olá NLW 05!",
+     })
+}); 
+
+app.post("/users", (request,response) =>{
+    return response.json({
+        message:"Usuário salvo com sucesso!",
+   })
+ });
+ * 
+ */
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,25 @@
-import express, { request, response } from "express";
-
+import express  from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
 import "./database";
-
 import { routes } from "./routes";
 
 const app = express();
+
+const http = createServer(app); //Criando protocolo http
+const io = new Server(http); //Criando protocolo ws
+
+io.on("connection",(socket: Socket) =>{
+    console.log("Se conectou", socket.id);
+});
 
 app.use(express.json());
 
 app.use(routes);
 
-// patch: Para alterar uma informação específica
+http.listen(3333, () => console.log(`Server is running on port:http://localhost:3333`));
 
 
-// app.get("/", (request, response) =>{
-//     return response.send("Olá NLW 05");
-// })
-
-
-// app.get("/", (request, response) => {
-//     return response.json({
-//         message: "Olá NLW 05!",
-//     })
-// }); 
-
-// app.post("/users", (request,response) =>{
-//     return response.json({
-//         message:"Usuário salvo com sucesso!",
-//     })
-// });
-app.listen(3333, () => console.log(`Server is running on port:http://localhost:3333`));
+// app.listen(3333, () => console.log(`Server is running on port:http://localhost:3333`));
 
